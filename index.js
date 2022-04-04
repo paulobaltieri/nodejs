c97f572801df526b8d3f7d5a7710ad03235eb854
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2022-04-04 20:16:14 -0300

    Eatudo com parametros

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Olá mundo')
 })
+app.get('/animal/:nome/:sobrenome', (req, res) => {
+    const nome = req.params.nome
+    const sobrenome = req.params.sobrenome
+    res.send(`Olá ${nome} ${sobrenome}`)
+})
+app.get('/raca/:nome?', (req, res) => {
+    const rac = req.params.nome
+    if (rac) {
+        res.send('Raça do animal')
+    } else {
+        res.send('A raça não foi informada')
+    }
 
-app.get('/raca', (req, res) => {
-    res.send('Raça do animal')
 })
 app.get('/raca/cor', (req, res) => {
     res.send('Cor do animal')
@@ -15,3 +25,6 @@ app.get('/raca/cor', (req, res) => {
 app.listen(5000, () => {
     console.log('Servido rodando...')
 })
+
+//req = Dados enviados pelo usuario
+//res = resposta que vai ser enviada para o usuario
