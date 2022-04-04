f5a5030460c6fe85e433d72d3282d2f6c1fe0c0c
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2022-04-04 19:28:59 -0300

    Criando rotas

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Olá mundo')
+})
+
+app.get('/raca', (req, res) => {
+    res.send('Raça do animal')
+})
+app.get('/raca/cor', (req, res) => {
+    res.send('Cor do animal')
+})
+
+app.listen(3000, () => {
+    console.log('Servido rodando...')
+})
